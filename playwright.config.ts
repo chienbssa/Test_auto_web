7ff1713b9de9b3ca6b46b87ea04f9dc809dcf8f3
chienbssa <chienbssa@gmail.com>
2026-04-19 18:09:25 +0700

    Update headless mode based on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://pythonweb-wymr.onrender.com',
-    headless: false,
+    headless: process.env.CI ? true : false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
